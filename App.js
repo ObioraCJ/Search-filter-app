57b611d1dd53daabd591b55e931cac676e10ae1e
Obiora Chukwuemeka Justice <83221540+ObioraCJ@users.noreply.github.com>
2022-06-02 16:27:35 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Counter from './React-projects/Counter';
+import TemperatureControl from './React-projects/Temp-ctrl-app';
+import SearchFilter from './React-projects/Search-filter';
+function App() {
+  return (
+    <div className='container'>
+      <SearchFilter />
+    </div>
+  )
+}
+
+export default App
